14ea3c03134d50fcb9af93c6a984aee8a3ca3e2f
29213 <50029213@iade.pt>
2019-11-09 23:20:04 +0000

    Classe Estoque

--- Sistama de gestão de inventarios/src/pt/iade/gestaoInventarios/models/Estoque.java
+++ Sistama de gestão de inventarios/src/pt/iade/gestaoInventarios/models/Estoque.java
@@ -3,47 +3,61 @@ package pt.iade.gestaoInventarios.models;
 import java.time.LocalDate;
 import java.util.List;
 
-
 public class Estoque {
 	private int idEstoque;
 	private LocalDate data;
 	private double valor;
-	private List<ItemDeInventario> itensDeInventario;
+	private List<ItemDeEstoque> itensDeEstoque;
 	private Colaborador colaborador;
+
+	public Estoque() {
+
+	}
+
 	public Estoque(int idEstoque, LocalDate data, double valor) {
 		this.idEstoque = idEstoque;
 		this.data = data;
 		this.valor = valor;
 	}
+
 	public int getIdEstoque() {
 		return idEstoque;
 	}
+
 	public void setIdEstoque(int idEstoque) {
 		this.idEstoque = idEstoque;
 	}
+
 	public LocalDate getData() {
 		return data;
 	}
+
 	public void setData(LocalDate data) {
 		this.data = data;
 	}
+
 	public double getValor() {
 		return valor;
 	}
+
 	public void setValor(double valor) {
 		this.valor = valor;
 	}
-	public List<ItemDeInventario> getItensDeInventario() {
+
+	public List<ItemDeEstoque> getItensDeInventario() {
 		return itensDeInventario;
 	}
-	public void setItensDeInventario(List<ItemDeInventario> itensDeInventario) {
+
+	public void setItensDeInventario(List<ItemDeEstoque> itensDeInventario) {
 		this.itensDeInventario = itensDeInventario;
 	}
+
 	public Colaborador getColaborador() {
 		return colaborador;
 	}
+
 	public void setColaborador(Colaborador colaborador) {
 		this.colaborador = colaborador;
 	}
-	
+
 }
--- Sistama de gestão de inventarios/src/pt/iade/gestaoInventarios/models/Produto.java
+++ Sistama de gestão de inventarios/src/pt/iade/gestaoInventarios/models/Produto.java
@@ -6,50 +6,61 @@ public class Produto {
 	private double preco;
 	private int quantidade;
 	private Categoria categiria;
-	
+
 	public Produto() {
-		
+
 	}
-	
+
 	public Produto(int idProduto, String nome, double preco, int quantidade) {
 		this.idProduto = idProduto;
 		this.nome = nome;
 		this.preco = preco;
 		this.quantidade = quantidade;
 	}
+
 	public int getIdProduto() {
 		return idProduto;
 	}
+
 	public void setIdProduto(int idProduto) {
 		this.idProduto = idProduto;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public double getPreco() {
 		return preco;
 	}
+
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
+
 	public int getQuantidade() {
 		return quantidade;
 	}
+
 	public void setQuantidade(int quantidade) {
 		this.quantidade = quantidade;
 	}
+
 	public Categoria getCategiria() {
 		return categiria;
 	}
+
 	public void setCategiria(Categoria categiria) {
 		this.categiria = categiria;
 	}
+
 	@Override
 	public String toString() {
 		return this.nome;
 	}
-	
+
 }
